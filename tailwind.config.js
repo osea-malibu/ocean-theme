3be71047a06bc1284d84afbc62a98f5462353c53
Erin Wilder <erin.wilder@oseamalibu.com>
2022-09-27 10:28:04 -0500

    thing

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
     "text-right",
     "xl:[--swiffy-slider-item-count:5]",
     "stamped-container",
-    "child:mb-1",
+    "child:mb-2",
     {
       pattern: /(bg|text|from|to)-(wave|seafoam|seaweed|sand|shell|coral)-(100|200|300|400|500|600|700|800)/,
       variants: ["xs", "sm", "md", "lg", "hover"],
