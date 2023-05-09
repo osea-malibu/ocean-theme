11d54da689ccac20d924a2a600e51a7335e3fe97
Erin Wilder <erin.wilder@oseamalibu.com>
2023-05-09 08:49:00 -0500

    added console log to global

--- assets/global.js
+++ assets/global.js
@@ -666,6 +666,7 @@ class VariantSelects extends HTMLElement {
   }
 
   onVariantChange() {
+    console.log("variant changed");
     this.updateOptions();
     this.updateMasterId();
     this.toggleAddButton(true, "", false);
