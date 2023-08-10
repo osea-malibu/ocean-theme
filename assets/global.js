5e199e6a25bb3a5bdf2d3e414c99ed6036a02c08
osea-malibu <93670936+osea-malibu@users.noreply.github.com>
2023-08-10 11:15:43 -0500

    lazy load multicolumn videos

--- assets/global.js
+++ assets/global.js
@@ -150,7 +150,7 @@ function onKeyUpEscape(event) {
 }
 
 // add border to header on scroll
-let observer = new IntersectionObserver((entries) => {
+let headerBorderObserver = new IntersectionObserver((entries) => {
   const headerWrapper = document.querySelector("#HeaderContent");
   if (entries[0].boundingClientRect.y < 0) {
     headerWrapper.classList.add("border-seaweed-300");
@@ -160,7 +160,29 @@ let observer = new IntersectionObserver((entries) => {
     headerWrapper.classList.add("border-transparent");
   }
 });
-observer.observe(document.querySelector("#HeaderScrollPixel"));
+headerBorderObserver.observe(document.querySelector("#HeaderScrollPixel"));
+
+const lazyVideos = [].slice.call(document.querySelectorAll("video.lazy"));
+let lazyVideoObserver = new IntersectionObserver((entries, observer) => {
+  entries.forEach((video) => {
+    if (video.isIntersecting) {
+      for (var source in video.target.children) {
+        var videoSource = video.target.children[source];
+        if (typeof videoSource.tagName === "string" && videoSource.tagName === "SOURCE") {
+          videoSource.src = videoSource.dataset.src;
+        }
+      }
+
+      video.target.load();
+      video.target.classList.remove("lazy");
+      lazyVideoObserver.unobserve(video.target);
+    }
+  });
+});
+
+lazyVideos.forEach((lazyVideo) => {
+  lazyVideoObserver.observe(lazyVideo);
+});
 
 class QuantityInput extends HTMLElement {
   constructor() {
