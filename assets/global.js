aae875a8e4e7a1012025dc0ec8f54e4bfdb803e9
osea-malibu <93670936+osea-malibu@users.noreply.github.com>
2024-02-15 09:33:32 -0600

    sticker bank js

--- assets/global.js
+++ assets/global.js
@@ -1414,6 +1414,47 @@ customElements.define("collection-video", CollectionVideo);
 class StickerBank extends HTMLElement {
   constructor() {
     super();
+
+    this.querySelectorAll("img").forEach((img) => {
+      img.addEventListener("mousedown", this.dragElement(img));
+    });
+  }
+
+  dragElement(element) {
+    let pos1 = 0;
+    let pos2 = 0;
+    let pos3 = 0;
+    let pos4 = 0;
+
+    element.onmousedown = dragMouseDown;
+
+    function dragMouseDown(e) {
+      e.preventDefault();
+      // get the mouse cursor position at startup:
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      // call a function whenever the cursor moves:
+      document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+      e.preventDefault();
+      // calculate the new cursor position:
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      // set the element's new position:
+      element.style.top = element.offsetTop - pos2 + "px";
+      element.style.left = element.offsetLeft - pos1 + "px";
+    }
+
+    function closeDragElement() {
+      // stop moving when mouse button is released:
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
   }
 }
 customElements.define("sticker-bank", StickerBank);
