4dd3ccee8a505c835b5fe96a17ea201a2da4d61f
osea-malibu <93670936+osea-malibu@users.noreply.github.com>
2024-05-23 19:35:51 -0500

    added filter functionality and added new memorial day template

--- assets/facets.js
+++ assets/facets.js
@@ -5,9 +5,10 @@ class FacetFiltersForm extends HTMLElement {
 
     this.debouncedOnSubmit = debounce((event) => {
       this.onSubmitHandler(event);
-    }, 500);
+    }, 800);
 
-    this.querySelector("form").addEventListener("input", this.debouncedOnSubmit.bind(this));
+    const facetForm = this.querySelector("form");
+    facetForm.addEventListener("input", this.debouncedOnSubmit.bind(this));
 
     const facetWrapper = this.querySelector("#FacetsWrapperDesktop");
     if (facetWrapper) facetWrapper.addEventListener("keyup", onKeyUpEscape);
@@ -15,7 +16,9 @@ class FacetFiltersForm extends HTMLElement {
 
   static setListeners() {
     const onHistoryChange = (event) => {
-      const searchParams = event.state ? event.state.searchParams : FacetFiltersForm.searchParamsInitial;
+      const searchParams = event.state
+        ? event.state.searchParams
+        : FacetFiltersForm.searchParamsInitial;
       if (searchParams === FacetFiltersForm.searchParamsPrev) return;
       FacetFiltersForm.renderPage(searchParams, null, false);
     };
@@ -32,16 +35,29 @@ class FacetFiltersForm extends HTMLElement {
     FacetFiltersForm.searchParamsPrev = searchParams;
     const sections = FacetFiltersForm.getSections();
     const countContainer = document.getElementById("ProductCount");
-    document.getElementById("ProductGridContainer").querySelector(".collection").classList.add("loading");
+    const countContainerDesktop = document.getElementById("ProductCountDesktop");
+    const loadingSpinners = document.querySelectorAll(
+      ".facets-container .loading__spinner, facet-filters-form .loading__spinner"
+    );
+    loadingSpinners.forEach((spinner) => spinner.classList.remove("hidden"));
+    document
+      .getElementById("ProductGridContainer")
+      .querySelector(".collection")
+      .classList.add("loading");
     if (countContainer) {
       countContainer.classList.add("loading");
+    }
+    if (countContainerDesktop) {
+      countContainerDesktop.classList.add("loading");
     }
 
     sections.forEach((section) => {
       const url = `${window.location.pathname}?section_id=${section.section}&${searchParams}`;
       const filterDataUrl = (element) => element.url === url;
 
-      FacetFiltersForm.filterData.some(filterDataUrl) ? FacetFiltersForm.renderSectionFromCache(filterDataUrl, event) : FacetFiltersForm.renderSectionFromFetch(url, event);
+      FacetFiltersForm.filterData.some(filterDataUrl)
+        ? FacetFiltersForm.renderSectionFromCache(filterDataUrl, event)
+        : FacetFiltersForm.renderSectionFromFetch(url, event);
     });
 
     if (updateURLHash) FacetFiltersForm.updateURLHash(searchParams);
@@ -56,6 +72,8 @@ class FacetFiltersForm extends HTMLElement {
         FacetFiltersForm.renderFilters(html, event);
         FacetFiltersForm.renderProductGridContainer(html);
         FacetFiltersForm.renderProductCount(html);
+        if (typeof initializeScrollAnimationTrigger === "function")
+          initializeScrollAnimationTrigger(html.innerHTML);
       });
   }
 
@@ -64,38 +82,117 @@ class FacetFiltersForm extends HTMLElement {
     FacetFiltersForm.renderFilters(html, event);
     FacetFiltersForm.renderProductGridContainer(html);
     FacetFiltersForm.renderProductCount(html);
+    if (typeof initializeScrollAnimationTrigger === "function")
+      initializeScrollAnimationTrigger(html.innerHTML);
   }
 
   static renderProductGridContainer(html) {
-    document.getElementById("ProductGridContainer").innerHTML = new DOMParser().parseFromString(html, "text/html").getElementById("ProductGridContainer").innerHTML;
+    document.getElementById("ProductGridContainer").innerHTML = new DOMParser()
+      .parseFromString(html, "text/html")
+      .getElementById("ProductGridContainer").innerHTML;
+
+    document
+      .getElementById("ProductGridContainer")
+      .querySelectorAll(".scroll-trigger")
+      .forEach((element) => {
+        element.classList.add("scroll-trigger--cancel");
+      });
   }
 
   static renderProductCount(html) {
-    const count = new DOMParser().parseFromString(html, "text/html").getElementById("ProductCount").innerHTML;
+    const count = new DOMParser()
+      .parseFromString(html, "text/html")
+      .getElementById("ProductCount").innerHTML;
     const container = document.getElementById("ProductCount");
+    const containerDesktop = document.getElementById("ProductCountDesktop");
     container.innerHTML = count;
     container.classList.remove("loading");
+    if (containerDesktop) {
+      containerDesktop.innerHTML = count;
+      containerDesktop.classList.remove("loading");
+    }
+    const loadingSpinners = document.querySelectorAll(
+      ".facets-container .loading__spinner, facet-filters-form .loading__spinner"
+    );
+    loadingSpinners.forEach((spinner) => spinner.classList.add("hidden"));
   }
 
   static renderFilters(html, event) {
     const parsedHTML = new DOMParser().parseFromString(html, "text/html");
+    const facetDetailsElementsFromFetch = parsedHTML.querySelectorAll(
+      "#FacetFiltersForm .js-filter, #FacetFiltersFormMobile .js-filter, #FacetFiltersPillsForm .js-filter"
+    );
+    const facetDetailsElementsFromDom = document.querySelectorAll(
+      "#FacetFiltersForm .js-filter, #FacetFiltersFormMobile .js-filter, #FacetFiltersPillsForm .js-filter"
+    );
 
-    const facetDetailsElements = parsedHTML.querySelectorAll("#FacetFiltersForm .js-filter, #FacetFiltersFormMobile .js-filter");
-    const matchesIndex = (element) => {
+    // Remove facets that are no longer returned from the server
+    Array.from(facetDetailsElementsFromDom).forEach((currentElement) => {
+      if (!Array.from(facetDetailsElementsFromFetch).some(({ id }) => currentElement.id === id)) {
+        currentElement.remove();
+      }
+    });
+
+    const matchesId = (element) => {
       const jsFilter = event ? event.target.closest(".js-filter") : undefined;
-      return jsFilter ? element.dataset.index === jsFilter.dataset.index : false;
+      return jsFilter ? element.id === jsFilter.id : false;
     };
-    const facetsToRender = Array.from(facetDetailsElements).filter((element) => !matchesIndex(element));
-    const countsToRender = Array.from(facetDetailsElements).find(matchesIndex);
 
-    facetsToRender.forEach((element) => {
-      document.querySelector(`.js-filter[data-index="${element.dataset.index}"]`).innerHTML = element.innerHTML;
+    const facetsToRender = Array.from(facetDetailsElementsFromFetch).filter(
+      (element) => !matchesId(element)
+    );
+    const countsToRender = Array.from(facetDetailsElementsFromFetch).find(matchesId);
+
+    facetsToRender.forEach((elementToRender, index) => {
+      const currentElement = document.getElementById(elementToRender.id);
+      // Element already rendered in the DOM so just update the innerHTML
+      if (currentElement) {
+        document.getElementById(elementToRender.id).innerHTML = elementToRender.innerHTML;
+      } else {
+        if (index > 0) {
+          const { className: previousElementClassName, id: previousElementId } =
+            facetsToRender[index - 1];
+          // Same facet type (eg horizontal/vertical or drawer/mobile)
+          if (elementToRender.className === previousElementClassName) {
+            document.getElementById(previousElementId).after(elementToRender);
+            return;
+          }
+        }
+
+        if (elementToRender.parentElement) {
+          document
+            .querySelector(`#${elementToRender.parentElement.id} .js-filter`)
+            .before(elementToRender);
+        }
+      }
     });
 
     FacetFiltersForm.renderActiveFacets(parsedHTML);
     FacetFiltersForm.renderAdditionalElements(parsedHTML);
 
-    if (countsToRender) FacetFiltersForm.renderCounts(countsToRender, event.target.closest(".js-filter"));
+    if (countsToRender) {
+      const closestJSFilterID = event.target.closest(".js-filter").id;
+
+      if (closestJSFilterID) {
+        FacetFiltersForm.renderCounts(countsToRender, event.target.closest(".js-filter"));
+        FacetFiltersForm.renderMobileCounts(
+          countsToRender,
+          document.getElementById(closestJSFilterID)
+        );
+
+        const newFacetDetailsElement = document.getElementById(closestJSFilterID);
+        const newElementSelector = newFacetDetailsElement.classList.contains(
+          "mobile-facets__details"
+        )
+          ? `.mobile-facets__close-button`
+          : `.facets__summary`;
+        const newElementToActivate = newFacetDetailsElement.querySelector(newElementSelector);
+
+        const isTextInput = event.target.getAttribute("type") === "text";
+
+        if (newElementToActivate && !isTextInput) newElementToActivate.focus();
+      }
+    }
   }
 
   static renderActiveFacets(html) {
@@ -118,20 +215,56 @@ class FacetFiltersForm extends HTMLElement {
       document.querySelector(selector).innerHTML = html.querySelector(selector).innerHTML;
     });
 
-    //document.getElementById("FacetFiltersFormMobile").closest("menu-drawer").bindEvents();
+    document.getElementById("FacetFiltersFormMobile").closest("menu-drawer").bindEvents();
   }
 
   static renderCounts(source, target) {
-    const targetElement = target.querySelector(".facets__selected");
-    const sourceElement = source.querySelector(".facets__selected");
+    const targetSummary = target.querySelector(".facets__summary");
+    const sourceSummary = source.querySelector(".facets__summary");
 
-    if (sourceElement && targetElement) {
-      target.querySelector(".facets__selected").outerHTML = source.querySelector(".facets__selected").outerHTML;
+    if (sourceSummary && targetSummary) {
+      targetSummary.outerHTML = sourceSummary.outerHTML;
+    }
+
+    const targetHeaderElement = target.querySelector(".facets__header");
+    const sourceHeaderElement = source.querySelector(".facets__header");
+
+    if (sourceHeaderElement && targetHeaderElement) {
+      targetHeaderElement.outerHTML = sourceHeaderElement.outerHTML;
+    }
+
+    const targetWrapElement = target.querySelector(".facets-wrap");
+    const sourceWrapElement = source.querySelector(".facets-wrap");
+
+    if (sourceWrapElement && targetWrapElement) {
+      const isShowingMore = Boolean(
+        target.querySelector("show-more-button .label-show-more.hidden")
+      );
+      if (isShowingMore) {
+        sourceWrapElement
+          .querySelectorAll(".facets__item.hidden")
+          .forEach((hiddenItem) => hiddenItem.classList.replace("hidden", "show-more-item"));
+      }
+
+      targetWrapElement.outerHTML = sourceWrapElement.outerHTML;
+    }
+  }
+
+  static renderMobileCounts(source, target) {
+    const targetFacetsList = target.querySelector(".mobile-facets__list");
+    const sourceFacetsList = source.querySelector(".mobile-facets__list");
+
+    if (sourceFacetsList && targetFacetsList) {
+      targetFacetsList.outerHTML = sourceFacetsList.outerHTML;
     }
   }
 
   static updateURLHash(searchParams) {
-    history.pushState({ searchParams }, "", `${window.location.pathname}${searchParams && "?".concat(searchParams)}`);
+    history.pushState(
+      { searchParams },
+      "",
+      `${window.location.pathname}${searchParams && "?".concat(searchParams)}`
+    );
   }
 
   static getSections() {
@@ -142,17 +275,49 @@ class FacetFiltersForm extends HTMLElement {
     ];
   }
 
+  createSearchParams(form) {
+    const formData = new FormData(form);
+    return new URLSearchParams(formData).toString();
+  }
+
+  onSubmitForm(searchParams, event) {
+    FacetFiltersForm.renderPage(searchParams, event);
+  }
+
   onSubmitHandler(event) {
     event.preventDefault();
-    const formData = new FormData(event.target.closest("form"));
-    const searchParams = new URLSearchParams(formData).toString();
-    FacetFiltersForm.renderPage(searchParams, event);
+    const sortFilterForms = document.querySelectorAll("facet-filters-form form");
+    if (event.srcElement.className == "mobile-facets__checkbox") {
+      const searchParams = this.createSearchParams(event.target.closest("form"));
+      this.onSubmitForm(searchParams, event);
+    } else {
+      const forms = [];
+      const isMobile = event.target.closest("form").id === "FacetFiltersFormMobile";
+
+      sortFilterForms.forEach((form) => {
+        if (!isMobile) {
+          if (
+            form.id === "FacetSortForm" ||
+            form.id === "FacetFiltersForm" ||
+            form.id === "FacetSortDrawerForm"
+          ) {
+            forms.push(this.createSearchParams(form));
+          }
+        } else if (form.id === "FacetFiltersFormMobile") {
+          forms.push(this.createSearchParams(form));
+        }
+      });
+      this.onSubmitForm(forms.join("&"), event);
+    }
   }
 
   onActiveFilterClick(event) {
     event.preventDefault();
     FacetFiltersForm.toggleActiveFacets();
-    const url = event.currentTarget.href.indexOf("?") == -1 ? "" : event.currentTarget.href.slice(event.currentTarget.href.indexOf("?") + 1);
+    const url =
+      event.currentTarget.href.indexOf("?") == -1
+        ? ""
+        : event.currentTarget.href.slice(event.currentTarget.href.indexOf("?") + 1);
     FacetFiltersForm.renderPage(url);
   }
 }
@@ -166,8 +331,10 @@ FacetFiltersForm.setListeners();
 class PriceRange extends HTMLElement {
   constructor() {
     super();
-    this.querySelectorAll("input").forEach((element) => element.addEventListener("change", this.onRangeChange.bind(this)));
-
+    this.querySelectorAll("input").forEach((element) => {
+      element.addEventListener("change", this.onRangeChange.bind(this));
+      element.addEventListener("keydown", this.onKeyDown.bind(this));
+    });
     this.setMinAndMaxValues();
   }
 
@@ -176,20 +343,28 @@ class PriceRange extends HTMLElement {
     this.setMinAndMaxValues();
   }
 
+  onKeyDown(event) {
+    if (event.metaKey) return;
+
+    const pattern =
+      /[0-9]|\.|,|'| |Tab|Backspace|Enter|ArrowUp|ArrowDown|ArrowLeft|ArrowRight|Delete|Escape/;
+    if (!event.key.match(pattern)) event.preventDefault();
+  }
+
   setMinAndMaxValues() {
     const inputs = this.querySelectorAll("input");
     const minInput = inputs[0];
     const maxInput = inputs[1];
-    if (maxInput.value) minInput.setAttribute("max", maxInput.value);
-    if (minInput.value) maxInput.setAttribute("min", minInput.value);
-    if (minInput.value === "") maxInput.setAttribute("min", 0);
-    if (maxInput.value === "") minInput.setAttribute("max", maxInput.getAttribute("max"));
+    if (maxInput.value) minInput.setAttribute("data-max", maxInput.value);
+    if (minInput.value) maxInput.setAttribute("data-min", minInput.value);
+    if (minInput.value === "") maxInput.setAttribute("data-min", 0);
+    if (maxInput.value === "") minInput.setAttribute("data-max", maxInput.getAttribute("data-max"));
   }
 
   adjustToValidValues(input) {
     const value = Number(input.value);
-    const min = Number(input.getAttribute("min"));
-    const max = Number(input.getAttribute("max"));
+    const min = Number(input.getAttribute("data-min"));
+    const max = Number(input.getAttribute("data-max"));
 
     if (value < min) input.value = min;
     if (value > max) input.value = max;
@@ -201,11 +376,19 @@ customElements.define("price-range", PriceRange);
 class FacetRemove extends HTMLElement {
   constructor() {
     super();
-    this.querySelector("a").addEventListener("click", (event) => {
+    const facetLink = this.querySelector("a");
+    facetLink.setAttribute("role", "button");
+    facetLink.addEventListener("click", this.closeFilter.bind(this));
+    facetLink.addEventListener("keyup", (event) => {
       event.preventDefault();
-      const form = this.closest("facet-filters-form") || document.querySelector("facet-filters-form");
-      form.onActiveFilterClick(event);
+      if (event.code.toUpperCase() === "SPACE") this.closeFilter(event);
     });
+  }
+
+  closeFilter(event) {
+    event.preventDefault();
+    const form = this.closest("facet-filters-form") || document.querySelector("facet-filters-form");
+    form.onActiveFilterClick(event);
   }
 }
 
