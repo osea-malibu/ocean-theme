8b8d44a3d97e3c2b1d1ee9f47bd06dac7b2f92bb
osea-malibu <93670936+osea-malibu@users.noreply.github.com>
2024-07-29 15:12:16 -0500

    pulled latest

--- assets/global.js
+++ assets/global.js
@@ -357,6 +357,286 @@ Shopify.CountryProvinceSelector.prototype = {
   },
 };
 
+class ShippingCalculator extends HTMLElement {
+  constructor() {
+    super();
+
+    this.zipForm = this.querySelector("form");
+    this.zipInput = this.querySelector("#shippingzip");
+    this.output = this.querySelector("#transitTimeOutput");
+
+    this.zipForm.addEventListener("submit", (e) => this.onZipSubmit(e));
+  }
+
+  onZipSubmit(e) {
+    e.preventDefault();
+
+    const { state, transitTime } = this.getStateData(this.zipInput.value);
+    const { transitionStart, transitionEnd } = this.output.dataset;
+
+    this.output.innerHTML = `${state}: ${transitTime}`;
+    this.output.classList.remove(...transitionStart.split(" "));
+    this.output.classList.add(...transitionEnd.split(" "));
+  }
+
+  getStateData(zipString) {
+    /* Ensure param is a string to prevent unpredictable parsing results */
+    if (typeof zipString !== "string") {
+      console.error("Must pass the zipcode as a string.");
+      return;
+    }
+
+    /* Ensure we have exactly 5 characters to parse */
+    if (zipString.length !== 5) {
+      console.error("Must pass a 5-digit zipcode.");
+      return;
+    }
+
+    /* Ensure we don't parse strings starting with 0 as octal values */
+    const zipcode = parseInt(zipString, 10);
+
+    let st;
+    let state;
+    let transitTime;
+
+    /* Code cases alphabetized by state */
+    if (zipcode >= 35000 && zipcode <= 36999) {
+      st = "AL";
+      state = "Alabama";
+      transitTime = "5-6 days";
+    } else if (zipcode >= 99500 && zipcode <= 99999) {
+      st = "AK";
+      state = "Alaska";
+      transitTime = "5-6 days";
+    } else if (zipcode >= 85000 && zipcode <= 86999) {
+      st = "AZ";
+      state = "Arizona";
+      transitTime = "2-3 days";
+    } else if (zipcode >= 71600 && zipcode <= 72999) {
+      st = "AR";
+      state = "Arkansas";
+      transitTime = "4-5 days";
+    } else if (zipcode >= 90000 && zipcode <= 96699) {
+      st = "CA";
+      state = "California";
+      transitTime = "2-3 days";
+    } else if (zipcode >= 80000 && zipcode <= 81999) {
+      st = "CO";
+      state = "Colorado";
+      transitTime = "3-4 days";
+    } else if ((zipcode >= 6000 && zipcode <= 6389) || (zipcode >= 6391 && zipcode <= 6999)) {
+      st = "CT";
+      state = "Connecticut";
+      transitTime = "5-6 days";
+    } else if (zipcode >= 19700 && zipcode <= 19999) {
+      st = "DE";
+      state = "Delaware";
+      transitTime = "5-6 days";
+    } else if (zipcode >= 32000 && zipcode <= 34999) {
+      st = "FL";
+      state = "Florida";
+      transitTime = "5-6 days";
+    } else if ((zipcode >= 30000 && zipcode <= 31999) || (zipcode >= 39800 && zipcode <= 39999)) {
+      st = "GA";
+      state = "Georgia";
+      transitTime = "4-5 days";
+    } else if (zipcode >= 96700 && zipcode <= 96999) {
+      st = "HI";
+      state = "Hawaii";
+      transitTime = "5-6 days";
+    } else if (zipcode >= 83200 && zipcode <= 83999 && zipcode != 83414) {
+      st = "ID";
+      state = "Idaho";
+      transitTime = "3-4 days";
+    } else if (zipcode >= 60000 && zipcode <= 62999) {
+      st = "IL";
+      state = "Illinois";
+      transitTime = "5-6 days";
+    } else if (zipcode >= 46000 && zipcode <= 47999) {
+      st = "IN";
+      state = "Indiana";
+      transitTime = "5-6 days";
+    } else if (zipcode >= 50000 && zipcode <= 52999) {
+      st = "IA";
+      state = "Iowa";
+      transitTime = "4-5 days";
+    } else if (zipcode >= 66000 && zipcode <= 67999) {
+      st = "KS";
+      state = "Kansas";
+      transitTime = "4-5 days";
+    } else if (zipcode >= 40000 && zipcode <= 42999) {
+      st = "KY";
+      state = "Kentucky";
+      transitTime = "4-5 days";
+    } else if (zipcode >= 70000 && zipcode <= 71599) {
+      st = "LA";
+      state = "Louisiana";
+      transitTime = "4-5 days";
+    } else if (zipcode >= 3900 && zipcode <= 4999) {
+      st = "ME";
+      state = "Maine";
+      transitTime = "5-6 days";
+    } else if (zipcode >= 20600 && zipcode <= 21999) {
+      st = "MD";
+      state = "Maryland";
+      transitTime = "5-6 days";
+    } else if ((zipcode >= 1000 && zipcode <= 2799) || zipcode == 5501 || zipcode == 5544) {
+      st = "MA";
+      state = "Massachusetts";
+      transitTime = "5-6 days";
+    } else if (zipcode >= 48000 && zipcode <= 49999) {
+      st = "MI";
+      state = "Michigan";
+      transitTime = "4-5 days";
+    } else if (zipcode >= 55000 && zipcode <= 56899) {
+      st = "MN";
+      state = "Minnesota";
+      transitTime = "4-5 days";
+    } else if (zipcode >= 38600 && zipcode <= 39999) {
+      st = "MS";
+      state = "Mississippi";
+      transitTime = "4-5 days";
+    } else if (zipcode >= 63000 && zipcode <= 65999) {
+      st = "MO";
+      state = "Missouri";
+      transitTime = "4-5 days";
+    } else if (zipcode >= 59000 && zipcode <= 59999) {
+      st = "MT";
+      state = "Montana";
+      transitTime = "4-5 days";
+    } else if (zipcode >= 68000 && zipcode <= 69999) {
+      st = "NE";
+      state = "Nebraska";
+      transitTime = "4-5 days";
+    } else if (zipcode >= 88900 && zipcode <= 89999) {
+      st = "NV";
+      state = "Nevada";
+      transitTime = "2-3 days";
+    } else if (zipcode >= 3000 && zipcode <= 3899) {
+      st = "NH";
+      state = "New Hampshire";
+      transitTime = "5-6 days";
+    } else if (zipcode >= 7000 && zipcode <= 8999) {
+      st = "NJ";
+      state = "New Jersey";
+      transitTime = "5-6 days";
+    } else if (zipcode >= 87000 && zipcode <= 88499) {
+      st = "NM";
+      state = "New Mexico";
+      transitTime = "3-4 days";
+    } else if (
+      (zipcode >= 10000 && zipcode <= 14999) ||
+      zipcode == 6390 ||
+      zipcode == 501 ||
+      zipcode == 544
+    ) {
+      st = "NY";
+      state = "New York";
+      transitTime = "5-6 days";
+    } else if (zipcode >= 27000 && zipcode <= 28999) {
+      st = "NC";
+      state = "North Carolina";
+      transitTime = "4-5 days";
+    } else if (zipcode >= 58000 && zipcode <= 58999) {
+      st = "ND";
+      state = "North Dakota";
+      transitTime = "3-4 days";
+    } else if (zipcode >= 43000 && zipcode <= 45999) {
+      st = "OH";
+      state = "Ohio";
+      transitTime = "4-5 days";
+    } else if ((zipcode >= 73000 && zipcode <= 73199) || (zipcode >= 73400 && zipcode <= 74999)) {
+      st = "OK";
+      state = "Oklahoma";
+      transitTime = "3-4 days";
+    } else if (zipcode >= 97000 && zipcode <= 97999) {
+      st = "OR";
+      state = "Oregon";
+      transitTime = "3-4 days";
+    } else if (zipcode >= 15000 && zipcode <= 19699) {
+      st = "PA";
+      state = "Pennsylvania";
+      transitTime = "5-6 days";
+    } else if (zipcode >= 300 && zipcode <= 999) {
+      st = "PR";
+      state = "Puerto Rico";
+      transitTime = "5-6 days";
+    } else if (zipcode >= 2800 && zipcode <= 2999) {
+      st = "RI";
+      state = "Rhode Island";
+      transitTime = "5-6 days";
+    } else if (zipcode >= 29000 && zipcode <= 29999) {
+      st = "SC";
+      state = "South Carolina";
+      transitTime = "4-5 days";
+    } else if (zipcode >= 57000 && zipcode <= 57999) {
+      st = "SD";
+      state = "South Dakota";
+      transitTime = "3-4 days";
+    } else if (zipcode >= 37000 && zipcode <= 38599) {
+      st = "TN";
+      state = "Tennessee";
+      transitTime = "4-5 days";
+    } else if (
+      (zipcode >= 75000 && zipcode <= 79999) ||
+      (zipcode >= 73301 && zipcode <= 73399) ||
+      (zipcode >= 88500 && zipcode <= 88599)
+    ) {
+      st = "TX";
+      state = "Texas";
+      transitTime = "3-4 days";
+    } else if (zipcode >= 84000 && zipcode <= 84999) {
+      st = "UT";
+      state = "Utah";
+      transitTime = "3-4 days";
+    } else if (zipcode >= 5000 && zipcode <= 5999) {
+      st = "VT";
+      state = "Vermont";
+      transitTime = "5-6 days";
+    } else if (
+      (zipcode >= 20100 && zipcode <= 20199) ||
+      (zipcode >= 22000 && zipcode <= 24699) ||
+      zipcode == 20598
+    ) {
+      st = "VA";
+      state = "Virginia";
+      transitTime = "4-5 days";
+    } else if (
+      (zipcode >= 20000 && zipcode <= 20099) ||
+      (zipcode >= 20200 && zipcode <= 20599) ||
+      (zipcode >= 56900 && zipcode <= 56999)
+    ) {
+      st = "DC";
+      state = "Washington DC";
+      transitTime = "4-5 days";
+    } else if (zipcode >= 98000 && zipcode <= 99499) {
+      st = "WA";
+      state = "Washington";
+      transitTime = "3-4 days";
+    } else if (zipcode >= 24700 && zipcode <= 26999) {
+      st = "WV";
+      state = "West Virginia";
+      transitTime = "5-6 days";
+    } else if (zipcode >= 53000 && zipcode <= 54999) {
+      st = "WI";
+      state = "Wisconsin";
+      transitTime = "4-5 days";
+    } else if ((zipcode >= 82000 && zipcode <= 83199) || zipcode == 83414) {
+      st = "WY";
+      state = "Wyoming";
+      transitTime = "4-5 days";
+    } else {
+      st = "none";
+      state = "none";
+      transitTime =
+        "Sorry, we could not find that zipcode in our database. Please contact Customer Care for shipping times.";
+    }
+
+    return { st, state, transitTime };
+  }
+}
+customElements.define("shipping-calculator", ShippingCalculator);
+
 class MenuDrawer extends HTMLElement {
   constructor() {
     super();
