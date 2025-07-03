4ec2d1ef6ec351c84af7af2427353d1af27278d3
osea-malibu <93670936+osea-malibu@users.noreply.github.com>
2025-07-02 21:19:24 -0500

    added plp interrupter tile

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
   safelist: [
     {
       pattern: /order-(1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20)/,
+      variants: ["xs", "sm", "md", "lg", "xl"],
     },
     {
       pattern: /w-(1\/6|1\/5|1\/4|1\/3|1\/2)/,
